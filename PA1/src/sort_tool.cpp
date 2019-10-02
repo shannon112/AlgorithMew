fc15afe4193c152d148ee5cff7ff183ec867ebc3
shannon112 <b03505011@ntu.edu.tw>
2019-10-02 15:36:42 +0800

    update raw PA1

---
+++ PA1/src/sort_tool.cpp
@@ -0,0 +1,81 @@
+// **************************************************************************
+//  File       [sort_tool.cpp]
+//  Author     [Yu-Hao Ho]
+//  Synopsis   [The implementation of the SortTool Class]
+//  Modify     [2019/9/6 Cheng-Yun Hsieh]
+// **************************************************************************
+
+#include "sort_tool.h"
+#include<iostream>
+
+// Constructor
+SortTool::SortTool() {}
+
+// Insertsion sort method
+void SortTool::InsertionSort(vector<int>& data) {
+    // Function : Insertion sort
+    // TODO : Please complete insertion sort code here
+}
+
+// Quick sort method
+void SortTool::QuickSort(vector<int>& data){
+    QuickSortSubVector(data, 0, data.size() - 1);
+}
+// Sort subvector (Quick sort)
+void SortTool::QuickSortSubVector(vector<int>& data, int low, int high) {
+    // Function : Quick sort subvector
+    // TODO : Please complete QuickSortSubVector code here
+    // Hint : recursively call itself
+    //        Partition function is needed
+}
+
+int SortTool::Partition(vector<int>& data, int low, int high) {
+    // Function : Partition the vector 
+    // TODO : Please complete the function
+    // Hint : Textbook page 171
+}
+
+// Merge sort method
+void SortTool::MergeSort(vector<int>& data){
+    MergeSortSubVector(data, 0, data.size() - 1);
+}
+
+// Sort subvector (Merge sort)
+void SortTool::MergeSortSubVector(vector<int>& data, int low, int high) {
+    // Function : Merge sort subvector
+    // TODO : Please complete MergeSortSubVector code here
+    // Hint : recursively call itself
+    //        Merge function is needed
+}
+
+// Merge
+void SortTool::Merge(vector<int>& data, int low, int middle1, int middle2, int high) {
+    // Function : Merge two sorted subvector
+    // TODO : Please complete the function
+}
+
+// Heap sort method
+void SortTool::HeapSort(vector<int>& data) {
+    // Build Max-Heap
+    BuildMaxHeap(data);
+    // 1. Swap data[0] which is max value and data[i] so that the max value will be in correct location
+    // 2. Do max-heapify for data[0]
+    for (int i = data.size() - 1; i >= 1; i--) {
+        swap(data[0],data[i]);
+        heapSize--;
+        MaxHeapify(data,0);
+    }
+}
+
+//Max heapify
+void SortTool::MaxHeapify(vector<int>& data, int root) {
+    // Function : Make tree with given root be a max-heap if both right and left sub-tree are max-heap
+    // TODO : Please complete max-heapify code here
+}
+
+//Build max heap
+void SortTool::BuildMaxHeap(vector<int>& data) {
+    heapSize = data.size(); // initialize heap size
+    // Function : Make input data become a max-heap
+    // TODO : Please complete BuildMaxHeap code here
+}
